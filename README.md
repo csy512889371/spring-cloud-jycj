e20c1a56a6270b2cedb3a29868072646cef4259b
csy512889371 <32060663+csy512889371@users.noreply.github.com>
2019-02-13 16:36:46 +0800

    配置修改
    配置修改

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1) euraka-server-jycj euraka 注册中心
 2) 
 
+
+# 启动脚本
+
+```shell
+java -jar -Xms500m -Xmx500m -Dspring.profiles.active=test euraka-server-jycj.jar
+```
---
+++ eureka-server-jycj/src/main/java/com/rjsoft/eurekaserver/listener/EurakeStatusChangeListener.java
@@ -0,0 +1,28 @@
+package com.rjsoft.eurekaserver.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
+import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRenewedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2018/8/20.
+ */
+@Slf4j
+@Component
+public class EurakeStatusChangeListener {
+    @EventListener
+    public void onApplicationEvent(EurekaInstanceCanceledEvent eurekaInstanceCanceledEvent) {
+        log.info("服务：{} 挂掉了",eurekaInstanceCanceledEvent.getAppName());
+    }
+    @EventListener
+    public void onApplicationEvent(EurekaInstanceRegisteredEvent eurekaInstanceRegisteredEvent) {
+        log.info("服务：{}， 注册成功了",eurekaInstanceRegisteredEvent.getInstanceInfo().getAppName()+"->节点:"+eurekaInstanceRegisteredEvent.getInstanceInfo().getIPAddr());
+    }
+    @EventListener
+    public void onApplicationEvent(EurekaInstanceRenewedEvent eurekaInstanceRenewedEvent) {
+        log.info("心跳检测服务：{}",eurekaInstanceRenewedEvent.getAppName());
+    }
+}
