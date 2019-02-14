a9878f1d606e8b3debc3b8635de606be20f03ae6
csy512889371 <32060663+csy512889371@users.noreply.github.com>
2019-02-14 14:56:12 +0800

    client

--- script/eureka-client.bat
+++ script/eureka-client.bat
@@ -1,6 +1,6 @@
 for /f "tokens=5" %%i in ('netstat -ano ^| findstr 8001 ^| findstr LISTENING') do (
 taskkill -pid  %%i -f
 )
-title api-gateway-zuul
+title eureka-client
 ping -n 3 127.0.0.1
 java -jar -Xms250m -Xmx250m  eureka-client-1.0-SNAPSHOT.jar
